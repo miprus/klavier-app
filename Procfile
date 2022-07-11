98ae63885ec7888902cec90138ed2955ebe95412
miprus <44868953+miprus@users.noreply.github.com>
2022-07-11 20:28:45 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn klavier_app:app
--- klavier_app.py
+++ klavier_app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
-import sqlite3
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'd267euqrhnw8kwgb'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'eddxjs9fz5rjyhf1'
+app.config['MYSQL_DATABASE_DB'] = 'li5xq3acpdr914e0'
+app.config['MYSQL_DATABASE_HOST'] = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'
+mysql.init_app(app)
 
 @app.route('/')
 def landing_page():
@@ -10,15 +16,15 @@ def landing_page():
 def music_library(category):
 	#checks if category is supplied in the url, if not - a default query is send to display all records with 'classic' tag assigned to them	
 	if category:
-		con = sqlite3.connect('klavier_app/klavierDB.db')
+		con = mysql.connect()
 		cur = con.cursor()
-		cur.execute("SELECT mp.pieceID, mp.piece_name, i.img_url FROM music_pieces mp, images i, tags t, tag_map tm WHERE mp.pieceID = i.pieceID AND t.tagID = tm.tagID AND mp.pieceID = tm.pieceID AND t.tag_name = ?", [category])
+		cur.execute("SELECT mp.pieceID, mp.piece_name, i.img_url FROM music_pieces mp, images i, tags t, tag_map tm WHERE mp.pieceID = i.pieceID AND t.tagID = tm.tagID AND mp.pieceID = tm.pieceID AND t.tag_name = (%s)", [category])
 		rows = cur.fetchall()
 		con.close() 
 
 		return render_template('music_library.html', rows = rows)
 	else:
-		con = sqlite3.connect('klavier_app/klavierDB.db')
+		con = psycopg2.connect()
 		cur = con.cursor()
 		cur.execute("SELECT mp.pieceID, mp.piece_name, i.img_url FROM music_pieces mp, images i, tags t, tag_map tm WHERE mp.pieceID = i.pieceID AND t.tagID = tm.tagID AND mp.pieceID = tm.pieceID AND t.tag_name = 'classic'")
 		rows = cur.fetchall()
@@ -32,9 +38,9 @@ def game_screen(pieceID):
 	#check if ID for music piece in the library is provided. If no ID is present the game screen page is displayed in 'free mode' and no JS level builder is called.
 	#If ID is present - SQL query data will be stored in hidden input. Its value will be raed by JS game engine and build level/load music piece based on the data 
 	if pieceID:
-		con = sqlite3.connect('klavier_app/klavierDB.db')
+		con = psycopg2.connect()
 		cur = con.cursor()  
-		cur.execute("SELECT piece_name, piece_url FROM music_pieces WHERE pieceID = ?", [pieceID])
+		cur.execute("SELECT piece_name, piece_url FROM music_pieces WHERE pieceID = (%s)", [pieceID])
 		pieceInfo = cur.fetchall()
 		con.close()  
 
